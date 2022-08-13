48e6266e360ea5199cc416669f6335d69c936124
Anush A <anushsaint03@gmail.com>
2022-08-13 14:35:56 +0530

    user-filter

--- user.js
+++ user.js
@@ -9,6 +9,7 @@ filter.addEventListener('input',(e)=> filterData(e.target.value))
 async function getData(){
     const res= await fetch('https://randomuser.me/api?results=50')
     const {results}= await res.json() 
+    console.log({results});
 
     result.innerHTML = ''
 
